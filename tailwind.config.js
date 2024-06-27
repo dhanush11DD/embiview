700b4fc3f7b034f0df745821acaa55070899ed7a
Dhanush16dd <dhanush11ynt@gmail.com>
2024-06-27 17:06:08 +0530

    fix for screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       flex: {
-        '2': '2 2 0%',
-        '3': '3 3 0%',
-        '4': '4 4 0%',
-        '5': '5 5 0%',
+        2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
+        5: "5 5 0%",
       },
       colors: {
         primary: "#4541FE",
         secondary: "#101722",
         white: "#FFFFFF",
-        "dark-text" : "#BCBFC3",
-        "light-text" : "#79818B",
-        "light-heading" : "#253241",
+        "dark-text": "#BCBFC3",
+        "light-text": "#79818B",
+        "light-heading": "#253241",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
         serif: ["Merriweather", "serif"],
-        lato: ["Lato", "sans-serif"]
+        lato: ["Lato", "sans-serif"],
       },
       container: {
         center: true,
         padding: "2rem",
+        screens: {
+          xs: "100%", // Custom max-width for xs screen
+          sm: "100%", // Custom max-width for sm screen
+          md: "720px", // Custom max-width for md screen
+          lg: "960px", // Custom max-width for lg screen
+          xl: "1140px", // Custom max-width for xl screen
+          "2xl": "1580px", // Custom max-width for 2xl screen
+        },
       },
       screens: {
         xs: "320px",
@@ -38,14 +43,12 @@ export default {
         "2xl": "1536px",
       },
       backgroundImage: {
-        'radial-gradient': 'radial-gradient(50% 50% at 50% 50%, #213B63 0%, #101722 100%)',
+        "radial-gradient":
+          "radial-gradient(50% 50% at 50% 50%, #213B63 0%, #101722 100%)",
         "primary-gradient":
           "linear-gradient(156deg, #0F1329 14.48%, #00050D 83.55%)",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
